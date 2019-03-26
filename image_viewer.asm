318770afb2b7c78075d1599ed772d9047d57f134
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-26 16:06:34 +0100

    reading image properties

--- image_viewer.asm
+++ image_viewer.asm
@@ -3,8 +3,10 @@ ASSUME cs:code1
 data1 segment
 image_name_len  db  0
 image_name      db  32 dup(0) 
-image_ptr       db  ?    
-image           db  200 dup(0)
+handle          db  ?    
+bitmap_header   db  14 dup(?)
+image_header    db  40 dup(?)
+file_ptr        dw  ?
 data1 ends
 
 code1 segment
@@ -23,30 +25,72 @@ start1:
     mov byte ptr es:[offset image_name_len], al
     mov cx, 0               
     mov cl, al
+    dec cl     ; workaround for additional sign in emulator
     rep movsb
     
-    ; file reading
+    ; file opening
     mov ax, seg image_name
     mov ds, ax
     mov dx, offset image_name
+    mov ax, 0
     mov ah, 3dh ; open
     int 21h
-    mov word ptr ds:[image_ptr], ax
-    mov dx, offset image
-    mov bx, word ptr ds:[image_ptr]
+    
+    ; bitmap header reading
+    mov word ptr ds:[handle], ax
+    mov dx, offset bitmap_header
+    mov bx, word ptr ds:[handle]
+    mov ax, 0
     mov ah, 3fh ; read
-    mov cx, 50
-    int 21h   
-    mov bx, word ptr ds:[image_ptr]
-    mov ah, 3eh ; close
+    mov cx, 14
     int 21h
     
+    ; file header reading
+    mov dx, offset image_header
+    mov ax, 0
+    mov ah, 3fh ; read
+    mov cx, 40
+    int 21h
     
+    ; point file to pixels and set file ptr
+    mov dx, word ptr ds:[bitmap_header + 10]  ; pixels offset
+    mov word ptr ds:[file_ptr], dx
+    call set_file_ptr
     
+    ; graphical mode
+    mov al, 13h
+    mov ah, 0
+    int 10h
+    
+    mov cx, 320
+lh: 
+    push cx
+    mov cx, 200
+lw: push cx
+    
+    pop cx
+    loop lw
+    add word ptr ds:[file_ptr]
+    pop cx    
+    loop lh       
+    
+    ; file closing   
+    mov bx, word ptr ds:[handle]
+    mov ax, 0
+    mov ah, 3eh ; close
+    int 21h
     
 end1: 
     mov ax, 4c00h
     int 21h
+    
+    
+set_file_ptr:
+    mov ah, 42h
+    mov al, 00
+    mov bx, word ptr ds:[handle]
+    int 21h
+    ret        
     
     
 code1 ends
